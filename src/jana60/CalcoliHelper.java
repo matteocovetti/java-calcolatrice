224c6d8d2bab303acbc993a671ebfdeafeca0e6e
matteocovetti <covetti.matteo@gmail.com>
2022-06-23 15:12:51 +0200

    add overload - bonus terminato

--- src/jana60/CalcoliHelper.java
+++ src/jana60/CalcoliHelper.java
@@ -12,6 +12,20 @@ public class CalcoliHelper {
 	 * metodi della vostra classe di helper (con un esempio per ogni casistica)
 	 */
 
+	/*
+	 * BONUS 1: per ogni metodo scrivetene una versione che esegue le stesse
+	 * operazioni ma su parametri double. Il fatto di dover scrivere lo stesso
+	 * metodo per tipi di parametro diversi applica nella pratica uno dei principi
+	 * di programmazione ad oggetti…Quale? Scrivetelo in un commento all’interno del
+	 * vostro codice!
+	 */
+
+	/*
+	 * Il principio della programmazione ad oggetti applicato nel bonus 1 è quello
+	 * dell'overload. Ovvero creare più metodi con lo stesso nome ma con parametri
+	 * diversi.
+	 */
+
 	// costruttore private
 	private CalcoliHelper() {
 
@@ -24,8 +38,18 @@ public class CalcoliHelper {
 		return x + y;
 	}
 
+	// overload somma
+	public static double somma(double x, double y) {
+		return x + y;
+	}
+
 	// metodo sottrazione
 	public static int sottrazione(int x, int y) {
+		return x - y;
+	}
+
+	// overload sottrazione
+	public static double sottrazione(double x, double y) {
 		return x - y;
 	}
 
@@ -38,13 +62,36 @@ public class CalcoliHelper {
 		}
 	}
 
+	// overload differenza
+	public static double differenza(double x, double y) {
+		if (x >= y) {
+			return x - y;
+		} else {
+			return y - x;
+		}
+	}
+
 	// metodo moltiplicazione
 	public static int moltiplicazione(int x, int y) {
 		return x * y;
 	}
 
+	// overload moltiplicazione
+	public static double moltiplicazione(double x, double y) {
+		return x * y;
+	}
+
 	// metodo valore assoluto
 	public static int valoreAssoluto(int x) {
+		if (x >= 0) {
+			return x;
+		} else {
+			return -x;
+		}
+	}
+
+	// overload valore assoluto
+	public static double valoreAssoluto(double x) {
 		if (x >= 0) {
 			return x;
 		} else {
@@ -61,8 +108,26 @@ public class CalcoliHelper {
 		}
 	}
 
+	// overload minimo tra due interi
+	public static double minimo(double x, double y) {
+		if (x >= y) {
+			return y;
+		} else {
+			return x;
+		}
+	}
+
 	// metodo massimo tra due interi
 	public static int massimo(int x, int y) {
+		if (x >= y) {
+			return x;
+		} else {
+			return y;
+		}
+	}
+
+	// overload massimo tra due interi
+	public static double massimo(double x, double y) {
 		if (x >= y) {
 			return x;
 		} else {
--- src/jana60/CalcoliTest.java
+++ src/jana60/CalcoliTest.java
@@ -6,9 +6,13 @@ public class CalcoliTest {
 
 		// prova metodo somma
 		System.out.println("La somma è: " + CalcoliHelper.somma(8, 9));
+		// prova metodo somma overload
+		System.out.println("La somma è: " + CalcoliHelper.somma(15.34, 14.25));
 
 		// prova sottrazione
 		System.out.println("La sottrazione è: " + CalcoliHelper.sottrazione(0, 10));
+		// prova sottrazione overload
+		System.out.println("La sottrazione è: " + CalcoliHelper.sottrazione(18.25, 10.20));
 
 		// prova differenza
 		System.out.println("La differenza è: " + CalcoliHelper.differenza(10, 2));
@@ -16,24 +20,44 @@ public class CalcoliTest {
 		System.out.println("La differenza è: " + CalcoliHelper.differenza(10, 10));
 		System.out.println("La differenza è: " + CalcoliHelper.differenza(10, -2));
 		System.out.println("La differenza è: " + CalcoliHelper.differenza(-2, 10));
+		// prova differenza overload
+		System.out.println("La differenza è: " + CalcoliHelper.differenza(15.96, 10.46));
+		System.out.println("La differenza è: " + CalcoliHelper.differenza(10.46, 15.96));
+		System.out.println("La differenza è: " + CalcoliHelper.differenza(15.96, 15.96));
+		System.out.println("La differenza è: " + CalcoliHelper.differenza(15.96, -10.46));
+		System.out.println("La differenza è: " + CalcoliHelper.differenza(-15.96, 10.46));
 
 		// prova moltiplicazione
 		System.out.println("La moltiplicazione è: " + CalcoliHelper.moltiplicazione(5, 9));
+		// prova moltiplicazione overload
+		System.out.println("La moltiplicazione è: " + CalcoliHelper.moltiplicazione(18.67, 3.15));
 
 		// prova valore assoluto
 		System.out.println("Il valore assoluto è: " + CalcoliHelper.valoreAssoluto(5));
 		System.out.println("Il valore assoluto è: " + CalcoliHelper.valoreAssoluto(-5));
+		System.out.println("Il valore assoluto è: " + CalcoliHelper.valoreAssoluto(0));
+		// prova valore assoluto overload
+		System.out.println("Il valore assoluto è: " + CalcoliHelper.valoreAssoluto(14.63));
+		System.out.println("Il valore assoluto è: " + CalcoliHelper.valoreAssoluto(-14.63));
 		System.out.println("Il valore assoluto è: " + CalcoliHelper.valoreAssoluto(0));
 
 		// prova minimo
 		System.out.println("Il valore minimo è: " + CalcoliHelper.minimo(10, 3));
 		System.out.println("Il valore minimo è: " + CalcoliHelper.minimo(2, 10));
 		System.out.println("Il valore minimo è: " + CalcoliHelper.minimo(9, 9));
+		// prova minimo overload
+		System.out.println("Il valore minimo è: " + CalcoliHelper.minimo(17.98, 6.54));
+		System.out.println("Il valore minimo è: " + CalcoliHelper.minimo(7.35, 7.68));
+		System.out.println("Il valore minimo è: " + CalcoliHelper.minimo(17.98, 17.98));
 
 		// prova massimo
 		System.out.println("Il valore massimo è: " + CalcoliHelper.massimo(10, 3));
 		System.out.println("Il valore massimo è: " + CalcoliHelper.massimo(2, 16));
 		System.out.println("Il valore massimo è: " + CalcoliHelper.massimo(18, 18));
+		// prova massimo overload
+		System.out.println("Il valore massimo è: " + CalcoliHelper.massimo(15.34, 6.44));
+		System.out.println("Il valore massimo è: " + CalcoliHelper.massimo(2.48, 9.54));
+		System.out.println("Il valore massimo è: " + CalcoliHelper.massimo(15.34, 15.34));
 	}
 
 }
