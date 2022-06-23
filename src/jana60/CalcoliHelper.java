40af96f0f7860ff18760494bab598798aca36793
matteocovetti <covetti.matteo@gmail.com>
2022-06-23 14:10:47 +0200

    first commit

---
+++ src/jana60/CalcoliHelper.java
@@ -0,0 +1,15 @@
+package jana60;
+
+public class CalcoliHelper {
+	/*
+	 * Creare una classe di helper CalcoliHelper. Come visto a lezione, strutturare
+	 * la classe in modo che non possa essere istanziata (cioè non posso creare
+	 * oggetti di tipo CalcoliHelper con la parola chiave new) e che presenti i
+	 * seguenti metodi static: - Somma di due numeri interi - Differenza tra due
+	 * numeri interi - Moltiplicazione di due numeri interi - Valore assoluto di un
+	 * numero intero - Minimo tra due numeri interi - Massimo tra due numeri interi
+	 * Create anche una classe CalcoliTest con il metodo main in cui testate tutti i
+	 * metodi della vostra classe di helper (con un esempio per ogni casistica)
+	 */
+
+}
